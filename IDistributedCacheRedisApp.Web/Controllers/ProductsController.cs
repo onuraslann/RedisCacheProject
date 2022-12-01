a210dc0d8bc2b317bb3bb26adccd6e0ba7374d51
theeaslann34@outlook.com <theeaslann34@outlook.com>
2022-12-01 15:52:00 +0300

    image dosyasını cache kaydetme

--- IDistributedCacheRedisApp.Web/Controllers/ProductsController.cs
+++ IDistributedCacheRedisApp.Web/Controllers/ProductsController.cs
@@ -1,5 +1,7 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using IDistributedCacheRedisApp.Web.Models;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Caching.Distributed;
+using Newtonsoft.Json;
 
 namespace IDistributedCacheRedisApp.Web.Controllers
 {
@@ -16,19 +18,42 @@ namespace IDistributedCacheRedisApp.Web.Controllers
         {
             DistributedCacheEntryOptions cacheEntryOptions = new DistributedCacheEntryOptions();
             cacheEntryOptions.AbsoluteExpiration = DateTime.Now.AddMinutes(1);
-           await distributedCache.SetStringAsync("name", "Fatih", cacheEntryOptions);
+            Product product = new Product{
+                 Id = 1,
+                  Name="Bilgisayar",
+                   Price=5000
+            };
+            string jsonProduct =JsonConvert.SerializeObject(product);
+            await distributedCache.SetStringAsync("product:1", jsonProduct,cacheEntryOptions);
+
+
             return View();
         }
         public async Task<IActionResult> Show()
         {
-            string name = await distributedCache.GetStringAsync("name");
-            ViewBag.name = name;
+            string jsonproduct = await distributedCache.GetStringAsync("product:1");
+            Product p = JsonConvert.DeserializeObject<Product>(jsonproduct);
+            ViewBag.product = p;
             return View();
         }
         public async Task<IActionResult> Remove()
         {
-            await distributedCache.RemoveAsync("name");
+            await distributedCache.RemoveAsync("product:1");
             return View();
+        }
+
+        public IActionResult ImageCache()
+        {
+            string path = Path.Combine(Directory.GetCurrentDirectory(), "wwwroot/images/pop-index-2020-chevrolet-corvette-c8-102-1571146873.jpg");
+            byte[] imageByte  =System.IO.File.ReadAllBytes(path);
+            distributedCache.Set("resim", imageByte);
+            return View();
+        }
+        public IActionResult ImageUrl()
+        {
+            byte[] resimbyte = distributedCache.Get("resim");
+
+            return File(resimbyte,"image/jpg");
         }
     }
 }
---
+++ IDistributedCacheRedisApp.Web/Models/Product.cs
@@ -0,0 +1,11 @@
+﻿namespace IDistributedCacheRedisApp.Web.Models
+{
+    public class Product
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public int Price { get; set; }
+    }
+}
